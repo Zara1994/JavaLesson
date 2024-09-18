e0a19fe2e5075accb261c22e647630df2238a44b
Zara1994 <avetisyan.zarine1994@gmail.com>
2024-09-18 15:48:20 +0500

    first commit

---
+++ src/test/java/com/example/LessonGradle/LessonGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LessonGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
